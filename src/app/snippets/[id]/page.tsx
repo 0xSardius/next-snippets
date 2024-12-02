5dbd1bbcc718bbab488b647c864fc307696fddee
Sardius <0xsardius@gmail.com>
2024-12-01 20:45:00 -0500

    SnippetShowPage update

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,25 @@
-export default function SnippetPage({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+import { notFound } from "next/navigation";
+import { db } from "../../db";
+
+interface SnippetShowPageProps {
+  params: { id: string };
+}
+
+export default async function SnippetShowPage({
+  params,
+}: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!snippet) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{snippet.title}</h1>
+      <pre>{snippet.code}</pre>
+    </div>
+  );
 }
