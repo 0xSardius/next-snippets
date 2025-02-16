64524a85f84513171ae0ee54e6298d9f2994aebc
Sardius <0xsardius@gmail.com>
2025-02-16 13:32:09 -0500

    createSnippet update

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const db = new PrismaClient();
-
-db.snippet.create({
-  data: {
-    title: "Hello World",
-    code: "const memecoinShit = () => {console.log('Memecoin shit')}",
-  },
-});
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,63 +1,59 @@
-import React from "react";
 import { db } from "@/app/db";
 import { redirect } from "next/navigation";
-function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // this needs to be a server action
-    "use server";
 
-    // Check the user's input and ensure it's valid
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-    // create a new record in the database
+export default function SnippetCreatePage() {
+  // Server action
+  async function createSnippet(formData: FormData) {
+    "use server";
+    //  Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    //Create new record in the database
     const snippet = await db.snippet.create({
       data: {
-        title,
-        code,
+        title: title as string,
+        code: code as string,
       },
     });
 
-    // redirect to the root page
-    redirect("/");
+    // Redirect to the snippet page
+    redirect(`/snippets/${snippet.id}`);
   }
 
   return (
     <form action={createSnippet}>
-      <h3 className="font-bold m-3">Create a Snippet</h3>
-      <div className="flex flex-col gap-4 max-w-lg mx-auto">
-        <div className="flex gap-4 items-center">
+      <h3 className="font-bold m-3">Create a new snippet</h3>
+      <div className="flex flex-col gap-4">
+        {/* Title */}
+        <div className="flex gap-4">
           <label className="w-12" htmlFor="title">
             Title
           </label>
           <input
-            type="text"
-            name="title"
             className="border rounded p-2 w-full"
             id="title"
+            type="text"
+            name="title"
           />
         </div>
 
-        <div className="flex gap-4 items-center">
+        {/* Code */}
+        <div className="flex gap-4">
           <label className="w-12" htmlFor="code">
             Code
           </label>
           <textarea
-            name="code"
             className="border rounded p-2 w-full"
             id="code"
-            rows={10}
+            name="code"
           />
         </div>
 
-        <button
-          type="submit"
-          className="rounded p-2 bg-blue-200 w-32 mx-auto mt-2"
-        >
+        <button type="submit" className="border rounded p-2 bg-blue-200">
           Create
         </button>
       </div>
     </form>
   );
 }
-
-export default SnippetCreatePage;
