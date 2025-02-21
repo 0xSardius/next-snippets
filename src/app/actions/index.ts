b2ab11d32a6cd56f541687e570db18d20abeaa59
Sardius <0xsardius@gmail.com>
2025-02-21 07:34:05 -0500

    snipper edit form update

---
+++ src/app/actions/index.ts
@@ -0,0 +1,8 @@
+"use server";
+// ^Designates all functions in this file as server actions
+
+import { db } from "@/app/db";
+
+export async function editSnippet() {
+  console.log("Editing snippet");
+}
--- src/app/components/snippet-edit-form.tsx
+++ src/app/components/snippet-edit-form.tsx
@@ -3,17 +3,18 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import { editSnippet } from "@/app/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
-function handleEditorChange(value: string | undefined) {
-  console.log(value);
-}
-
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   const [code, setCode] = useState(snippet.code);
+
+  function handleEditorChange(value: string | undefined) {
+    console.log(value);
+  }
 
   return (
     <div>
